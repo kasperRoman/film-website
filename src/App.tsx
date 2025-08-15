95b6347af2eb34f1703b26bd30e6c3d9db95a560
Roman <kaspryshyn_roman@ukr.net>
2025-08-15 15:36:43 +0300

    chore: add global css reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './App.scss';
 
 
 function App() {
   
   return (
-    <div >
-
+    <div  >
+        app
     </div>
   );
 }
--- src/services/api.servise.ts
+++ src/services/api.servise.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+
 const axiosInstance = axios.create({
     baseURL:'https://api.themoviedb.org/3',
     params:{
