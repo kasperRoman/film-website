838249a19a3b2868bef9a79480721fdf90183de6
Roman <kaspryshyn_roman@ukr.net>
2025-08-15 10:18:13 +0300

    chore: install axios, sass, react-router-dom, swiper, query-string

---
+++ src/services/api.servise.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL:'https://api.themoviedb.org/3',
+    params:{
+        api_key:process.env.REACT_APP_TMDB_KEY
+    }
+})
