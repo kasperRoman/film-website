fde7b64f258119dad13828594eb51348e97937b2
Roman <kaspryshyn_roman@ukr.net>
2025-08-15 16:38:12 +0300

    chore: add apiConfig , axiosClient

--- src/services/api.servise.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-const axiosInstance = axios.create({
-    baseURL:'https://api.themoviedb.org/3',
-    params:{
-        api_key:process.env.REACT_APP_TMDB_KEY
-    }
-})
