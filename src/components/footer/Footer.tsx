46e3a2381fe2d8ee1b270a8e95ed58bf8d797802
Roman <kaspryshyn_roman@ukr.net>
2025-08-23 11:28:37 +0300

    add footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import styles from "./footer.module.scss";
+import bg from "../../assets/images/footer-bg.jpg";
+import logo from "../../assets/images/tmovie.png";
+import clsx from "clsx";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.footer} style={{ backgroundImage: `url(${bg})` }}>
+      <div className={clsx(styles.content, "container")}>
+        <div className={styles.logoContent}>
+          <div className={styles.logo}>
+            <img src={logo} alt="Movies logo" />
+            <Link to="/">Movies</Link>
+          </div>
+        </div>
+        <div className={styles.menuContent}>
+          <div className={styles.menu}>
+            <Link to="/">Home</Link>
+            <Link to="/">Contact us</Link>
+            <Link to="/">Term of services</Link>
+            <Link to="/">About us</Link>
+          </div>
+          <div className={styles.menu}>
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Premium</Link>
+            <Link to="/">Privacy policy</Link>
+          </div>
+          <div className={styles.menu}>
+            <Link to="/">You must watch</Link>
+            <Link to="/">Contact us</Link>
+            <Link to="/">Top IMDB</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
